1ff8c39e385304b48d21df5237ded4ba2bfbe736
Liz Caruso <caruso.elizabeth@gmail.com>
2022-09-24 18:39:26 -0700

    set up event listener that increments times fed by 1 per click

--- feed-cat.js
+++ feed-cat.js
@@ -1,1 +1,16 @@
 console.log('hello world')
+
+// Define a starting variable with 0 value
+
+let timesFed = 0; 
+
+// Define variable for the cat food can image
+
+let feedCat = document.querySelector('#cat-food-can')
+
+// Set up an event listener for clicks of cat food can image
+
+feedCat.addEventListener('click', function() {
+    timesFed +=1
+    console.log(timesFed)
+});
