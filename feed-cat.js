15de3d3ef51c109556b4d4b024bb2ca19d2a7a26
Liz Caruso <caruso.elizabeth@gmail.com>
2022-09-25 12:30:39 -0700

    wrapping up styling on index html just need a few more adjustments to button links

--- feed-cat.js
+++ feed-cat.js
@@ -16,17 +16,22 @@ let needsFood = document.querySelector('#hasnt-been-fed-cat')
 
 let fedCat = document.querySelector('#fed-cat')
 
+// Define variable for the full cat
+
+let fullCat = document.querySelector('#full-cat')
+
 // Set up an event listener for clicks of cat food can image
 
 feedCat.addEventListener('click', function() {
     timesFed +=1
     console.log(timesFed)
     if (timesFed >= 1) {
-        fedCat.style.opacity=1
-        needsFood.style.opacity=-1
+        fedCat.style.display = "block"
+        needsFood.style.display = "none"
     }
-    else {
-        fedCat.style.opacity=-1
-        needsFood.style.opacity=1
+    else if (timesFed > 3) {
+        fedCat.style.display = "none"
+        needsFood.style.display = "none"
+        fullCat.style.display = "block"
     }
 });
