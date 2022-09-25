95fbc4fd05bf76ae31b2ad727abd22320586501a
Liz Caruso <caruso.elizabeth@gmail.com>
2022-09-24 18:49:42 -0700

    adding in some if else logic with the intent of hiding or showing images

--- feed-cat.js
+++ feed-cat.js
@@ -8,9 +8,25 @@ let timesFed = 0;
 
 let feedCat = document.querySelector('#cat-food-can')
 
+// Define variable for the cat that hasn't been fed
+
+let needsFood = document.querySelector('#hasnt-been-fed-cat')
+
+// Define variable for the cat that has been fed
+
+let fedCat = document.querySelector('#fed-cat')
+
 // Set up an event listener for clicks of cat food can image
 
 feedCat.addEventListener('click', function() {
     timesFed +=1
     console.log(timesFed)
+    if (timesFed >= 1) {
+        fedCat.style.opacity=1
+        needsFood.style.opacity=-1
+    }
+    else {
+        fedCat.style.opacity=-1
+        needsFood.style.opacity=1
+    }
 });
