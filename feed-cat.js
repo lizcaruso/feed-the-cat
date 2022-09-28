ca06a197fcc811f376ebf7fe9fddef4c5d59449f
Liz Caruso <caruso.elizabeth@gmail.com>
2022-09-27 18:37:31 -0700

    javascript functionality is complete

--- feed-cat.js
+++ feed-cat.js
@@ -1,5 +1,3 @@
-console.log('hello world')
-
 // Define a starting variable with 0 value
 
 let timesFed = 0; 
@@ -12,13 +10,21 @@ let feedCat = document.querySelector('#cat-food-can')
 
 let counter = document.querySelector('.fed-cat-counter')
 
-// Define variable for the cat that hasn't been fed
+// Define a variable for the image that displays when the cat is not fed
 
-let needsFood = document.querySelector('#cat-not-fed-content')
+let unfedImage = document.querySelector('#hasnt-been-fed-cat')
 
-// Variable for the counter text
+// Define a variable for the image that displays when the cat is fed 1-3 times
 
-let counterContent = document.querySelector("#cat-being-fed-content")
+let fedImage = document.querySelector('#has-been-fed-cat')
+
+// Define a variable for the content that displays when the cat is fed 4 times
+
+let fullCat = document.querySelector('#full-cat')
+
+// Variable for the cat meow that plays when user clicks the can
+
+const catMeow = new Audio("Meow-cat-sound-effect.mp3")
 
 // Set up an event listener for clicks of cat food can image
 // Add incremental counter
@@ -26,7 +32,33 @@ let counterContent = document.querySelector("#cat-being-fed-content")
 
 feedCat.addEventListener('click', function() {
     timesFed +=1
+    catMeow.play()
     if (timesFed >= 1) {
         counter.innerText = timesFed
+        unfedImage.style.display = "none";
+        fedImage.style.display = "block";
+    }
+    if (timesFed >= 4) {
+        fullCat.style.display = "block";
+        fedImage.style.display = "none";
+        document.getElementById('cat-not-fed-content').style.display = "none";
     }
 });
+
+
+
+// // Define variable for the cat that hasn't been fed
+
+// let needsFood = document.querySelector('#cat-not-fed-content')
+
+// // Variable for the counter text
+
+// let counterContent = document.querySelector("#cat-being-fed-content")
+
+// // Variable for the content if cat has been fed 1 - 3 times
+
+// let catFed = document.querySelector("#cat-was-fed")
+
+// // Variable for content if cat has been fed 4 or more times.
+
+// let fullCat = document.querySelector("#full-cat")
